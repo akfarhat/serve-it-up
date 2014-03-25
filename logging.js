96953bc34baa1bb96f74c7230367b5c0627ac01b
AK <ak.f@live.com>
2014-03-25 11:29:27 -0400

    Fixed some logging bugs

--- logging.js
+++ logging.js
@@ -8,14 +8,32 @@ function log (message) {
 }
 
 $('document').ready(function () {
-	var selectors = '.btn, #searchInput, #categoryContainer a';
-	$(selectors).on('click', function (e) {
-		var id = e.target.id;
+	
+	$('#categoryContainer a').on('click', function (e) {
+		var id = 'Category: ' + this.innerHTML();
 		var currentDate = '[' + new Date().toUTCString() + '] ';
 		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
 	});
 	
-	$('.carousel-caption a').on('click', function (e) {
+	$('#searchInput').on('click', function (e) {
+		var id = 'Search field';
+		var currentDate = '[' + new Date().toUTCString() + '] ';
+		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
+	});
+	
+	$('#assistBtn').on('click', function (e) {
+		var id = 'Assistance button';
+		var currentDate = '[' + new Date().toUTCString() + '] ';
+		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
+	});
+	
+	$('#helpBtn, #helpBtn .glyphicon').on('click', function (e) {
+		var id = 'Help button';
+		var currentDate = '[' + new Date().toUTCString() + '] ';
+		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
+	});
+	
+	$('.carousel-caption').on('click', function (e) {
 		var id = 'Featured item';
 		var currentDate = '[' + new Date().toUTCString() + '] ';
 		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
@@ -23,6 +41,44 @@ $('document').ready(function () {
 	
 	$('.breadcrumb a').on('click', function (e) {
 		var id = e.target.html() + ' breadcrumb';
+		var currentDate = '[' + new Date().toUTCString() + '] ';
+		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
+	});
+	
+	$('#menuItemContainer .list-group-item .btn, #menuItemContainer .list-group-item .btn .glyphicon').on('click', function (e) {
+		if($(e.target).is('.btn')) {
+			var id = 'Add button ' + e.target.id;
+		}
+		else {
+			var id = 'Duplicate: Add button ' + $(e.target).parent().attr('id');
+		}
+		var currentDate = '[' + new Date().toUTCString() + '] ';
+		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
+		
+	});
+	
+	$('#currentOrderContainer .btn, #currentOrderContainer .glyphicon').on('click', function (e) {
+		if($(e.target).is('.btn')) {
+			var id = 'Remove button ' + e.target.id;
+		}
+		else {
+			var id = 'Duplicate: Add button ' + $(e.target).parent().attr('id');
+		}
+			
+		var currentDate = '[' + new Date().toUTCString() + '] ';
+		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
+	});
+	
+	$('#placeOrderButton').on('click', function (e) {
+		var id = 'Place Order button';
+			
+		var currentDate = '[' + new Date().toUTCString() + '] ';
+		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
+	});
+	
+	$('#checkoutButton').on('click', function (e) {
+		var id = 'Checkout button';
+			
 		var currentDate = '[' + new Date().toUTCString() + '] ';
 		log(currentDate + id + ' clicked at position x:' + e.pageX + ' y: ' + e.pageY);
 	});
